136897d648ef8e782974c5c682ada6a28c8a187b
Александр Некрасов <lxmrrr@gmail.com>
2016-02-19 17:58:47 +0300

    верстка слайдера

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@ $(document).ready(function(){
   $('.main-slider__wrapper').slick({
 		autoplay:true,
 		dots:true,
-		fade:true
+		fade:true,
+		speed: 500,
+		cssEase: 'linear',
+		autoplaySpeed:10000
   });
 	$('.featured-products__container').slick({
 		autoplay:true,
 		dots:true,
 		slidesToShow:4,
-		slidesToScroll:4
+		slidesToScroll:4,
+		autoplaySpeed:10000
   });
 });
