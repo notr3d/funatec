f2aee9d7bf9ab22fc71de8efa8a77966eed14495
Александр Некрасов <lxmrrr@gmail.com>
2016-02-20 16:27:40 +0300

    верстка главной и начало раздела "контакты"

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('.main-slider__wrapper').slick({
 		autoplay:true,
 		dots:true,
+		arrows:false,
 		fade:true,
 		speed: 500,
 		cssEase: 'linear',
@@ -12,6 +13,67 @@ $(document).ready(function(){
 		dots:true,
 		slidesToShow:4,
 		slidesToScroll:4,
-		autoplaySpeed:10000
+		autoplaySpeed:10000,
+		responsive: [
+			{
+				breakpoint: 1280,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},            
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
   });
 });
+
+{
+
+    var myCenter=new google.maps.LatLng(54.212117, 37.646803);
+
+    function initialize(){
+        var mapProp = {
+            center:myCenter,
+            zoom:16,
+					  scrollwheel:false,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+
+        var map=new google.maps.Map(document.getElementById("map"), mapProp);
+
+        var marker=new google.maps.Marker({
+            position:myCenter,
+            icon:'../img/page-logo.png',
+            size: new google.maps.Size(20, 32)
+        });
+
+        marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+};
