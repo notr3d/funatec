3882f8791d8c09b282c3f4203758cd6303f77946
Александр Некрасов <lxmrrr@gmail.com>
2016-02-25 16:03:37 +0300

    мелкие исправления

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ $(document).ready(function(){
 	});
 	
 	$('.product__slider-nav').slick({
-		slidesToShow: 3,
+		//slidesToShow: 3,
 		slidesToScroll: 1,
 		asNavFor: '.product__slider',
 		dots: true,
@@ -92,28 +92,21 @@ $(document).ready(function(){
 });
 
 {
-
-    var myCenter=new google.maps.LatLng(54.212117, 37.646803);
-
-    function initialize(){
-        var mapProp = {
-            center:myCenter,
-            zoom:16,
-					  scrollwheel:false,
-            mapTypeId:google.maps.MapTypeId.ROADMAP
-        };
-
-        var map=new google.maps.Map(document.getElementById("map"), mapProp);
-
-        var marker=new google.maps.Marker({
-            position:myCenter,
-            icon:'../img/page-logo.png',
-            size: new google.maps.Size(20, 32)
-        });
-
-        marker.setMap(map);
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-
+	var myCenter=new google.maps.LatLng(54.212117, 37.646803);
+	function initialize(){
+	var mapProp = {
+		center:myCenter,
+		zoom:16,
+		scrollwheel:false,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	var map=new google.maps.Map(document.getElementById("map"), mapProp);
+	var marker=new google.maps.Marker({
+		position:myCenter,
+		icon:'../img/page-logo.png',
+		size: new google.maps.Size(20, 32)
+	});
+	marker.setMap(map);
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
 };
